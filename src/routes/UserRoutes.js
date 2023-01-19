e0859afa76d94223af35d8a7a10d71aa006a5fee
Emirhan <emiroguz98@gmail.com>
2023-01-19 10:27:17 +0300

    add credential verification in signin req

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const AuthMiddlewares = require('../middlewares/auth');
 
-router.post('/signup', AuthMiddlewares.verifySignupRequest, UserController.signUp);
-router.post('/signin', UserController.signin);
+router.post('/signup', AuthMiddlewares.verifyCredentials, AuthMiddlewares.verifySignupRequest, UserController.signUp);
+router.post('/signin', AuthMiddlewares.verifyCredentials, UserController.signin);
 
 module.exports = router;
